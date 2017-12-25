c77b892a0f1176669b5ce94a17f23fb6bf45ca1e
extrabin <33796010+extrabin@users.noreply.github.com>
2017-12-25 10:55:36 +0900

    qqqq

--- MyGitProject/src/aaa/bbb/HelloWorld3.java
+++ MyGitProject/src/aaa/bbb/HelloWorld3.java
@@ -5,6 +5,8 @@ public class HelloWorld3 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello3");
+		
+		System.out.println("Hello3_1");
 	}
 
 }
